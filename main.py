87facd7ab507992d3c4b7c1cdcdabfe437aeb4ea
Syarif Ys <5yarif.y.s@gmail.com>
2022-09-04 11:26:38 +0800

    update 4 september 2022

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ Aplikasi deteksi gempa terkini
 
 
 def ekstraksi_data():
-
     """
     Tanggal: 04 September 2022
     Waktu: 04:10:14 WIB
@@ -20,7 +19,7 @@ def ekstraksi_data():
     hasil["waktu"] = "04:10:14 WIB"
     hasil["magnitudo"] = 3.1
     hasil["kedalaman"] = "9 Km"
-    hasil["lokasie"] = {"LS":7.22, "BT":109.89}
+    hasil["lokasi"] = {"ls":7.22, "bt":109.89}
     hasil["pusat"] = "Pusat gempa berada di darat 16 km Barat Laut WONOSOBO"
     hasil["dirasakan"] = "Dirasakan (Skala MMI): II Dieng, II Banjarnegara, II Bawang (Kab. Batang)"
 
@@ -29,6 +28,14 @@ def ekstraksi_data():
 
 
 def tampilkan_data(result):
+    print("Gempa Terakhir berdasarkan BMKG")
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['tanggal']}")
+    print(f"Magnitudo {result['waktu']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
+    print(f"pusat {result['pusat']}")
+    print(f"Dirasakan {result['dirasakan']}")
     pass
 
 

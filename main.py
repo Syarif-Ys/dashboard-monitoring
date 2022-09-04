599d978a9084bc04fb53e7e38d817ff72e3738c5
Syarif Ys <5yarif.y.s@gmail.com>
2022-09-04 11:07:57 +0800

    update 4 september 2022

--- main.py
+++ main.py
@@ -4,6 +4,27 @@ Aplikasi deteksi gempa terkini
 
 
 def ekstraksi_data():
+
+    """
+    Tanggal: 04 September 2022
+    Waktu: 04:10:14 WIB
+    Magnitudo: 3.1
+    Kedalaman: 9 km
+    Lokasi: LS=7.22  BT=109.89
+    Pusat Gempa: Pusat gempa berada di darat 16 km Barat Laut WONOSOBO
+    Dirasakan: Dirasakan (Skala MMI): II Dieng, II Banjarnegara, II Bawang (Kab. Batang)
+    :return:
+    """
+    hasil = dict()
+    hasil["tanggal"] = "04 September 2022"
+    hasil["waktu"] = "04:10:14 WIB"
+    hasil["magnitudo"] = 3.1
+    hasil["kedalaman"] = "9 Km"
+    hasil["lokasie"] = {"LS":7.22, "BT":109.89}
+    hasil["pusat"] = "Pusat gempa berada di darat 16 km Barat Laut WONOSOBO"
+    hasil["dirasakan"] = "Dirasakan (Skala MMI): II Dieng, II Banjarnegara, II Bawang (Kab. Batang)"
+
+    return hasil
     pass
 
 
